487c24e9dca59ceb2032aafa951313ecc576fb14
Ravi Silva <ravifelipee@gmail.com>
2023-05-27 07:31:31 -0300

    substituição da tag '<a/>' pela tag '<Link/>' para dar seguimento no padrão SPA

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import styles from './Menu.module.css'
+import { Link } from 'react-router-dom'
 
 export default function Menu() {
     return (
         <header>
             <nav className={styles.navegacao}>
-                <a className={styles.link} href='/'>Início</a>
-                <a className={styles.link} href='/sobre-mim'>Sobre Mim</a>
+                <Link className={styles.link} to='/'>Início</Link>
+                <Link className={styles.link} to='/sobre-mim'>Sobre Mim</Link>
             </nav>
         </header>
     )
 }
+
+
+//Implementação do "LINK" para que não seja feito o "RELOAD" da página e consequentemente seguindo um padrão de Single Page Aplication (SPA).
+
+//ao invês de usar <a href="">
+//usar o <Link to="">
+
+//isso evita o "RELOAD" da página sempre que for selecionando alguma opção do 'Menu'.
+//OBS: O react-router-dom facilita a criação de SPA's
