d0dd048802840a12dedc9561382ae0c45320c064
Ravi Silva <ravifelipee@gmail.com>
2023-06-04 10:47:47 -0300

    estilização do 'MENU' usando o 'useLocation'

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import styles from './Menu.module.css'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 export default function Menu() {
+    const localizacao = useLocation();
+
+    console.log(localizacao);
+
     return (
         <header>
             <nav className={styles.navegacao}>
-                <Link className={styles.link} to='/'>Início</Link>
-                <Link className={styles.link} to='/sobre-mim'>Sobre Mim</Link>
+                <Link className={`
+                        ${styles.link} 
+                        ${localizacao.pathname === '/' ? styles.linkDestacado : ""}
+                    `} to='/'>
+                    Início
+                </Link>
+                <Link className={`
+                        ${styles.link}
+                        ${localizacao.pathname === '/sobre-mim' ? styles.linkDestacado : ""}
+                    `} to='/sobre-mim'>
+                    Sobre Mim
+                </Link>
             </nav>
         </header>
     )
@@ -21,3 +35,7 @@ export default function Menu() {
 
 //isso evita o "RELOAD" da página sempre que for selecionando alguma opção do 'Menu'.
 //OBS: O react-router-dom facilita a criação de SPA's
+
+//---
+
+//O useLocation() é um HOOK do react-router-dom que vai retornar alguma informação
