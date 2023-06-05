059a54bac021d39469fc48190cc388ac4383d577
Ravi Silva <ravifelipee@gmail.com>
2023-06-05 15:47:41 -0300

    implementação da página de 'Posts'

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,13 +1,37 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import posts from 'json/posts.json'
+import PostModelo from 'componentes/PostModelo';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import './Post.css'
+
 
 export default function Post() {
     const parametros = useParams();
-    console.log(parametros);
+    const post = posts.find((post) => {
+        return post.id === Number(parametros.id);
+    })
+
+    if (!post) {
+        return (
+            <h1>Post Não Encontrado..</h1>
+        )
+    }
 
     return (
-        <div>
-            Post {parametros.id}
-        </div>
+        <PostModelo
+            fotoCapa={`/assets/posts/${post.id}/capa.png`}
+            titulo={post.titulo}>
+            <div className='post-markdown-container'>
+                <ReactMarkdown>
+                    {post.texto}
+                </ReactMarkdown>
+            </div>
+        </PostModelo>
     )
 }
+
+
+// foi usado o metodo 'find()' para encontrar o post que está relacionado com o parametro da URL
+//na implementação do metodo, é comparado o parametro com o id que está no arquivo json importado
+//depois disso é retornado o POST correspondente
