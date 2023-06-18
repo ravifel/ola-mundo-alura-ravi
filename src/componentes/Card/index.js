a25ffdaf112e033d38b566626885413216a74b8a
Ravi Silva <ravifelipee@gmail.com>
2023-06-18 09:14:26 -0300

    implementação de valor limite para caracteres da descrição dos Cards e implementções de estilização

--- src/componentes/Card/index.js
+++ src/componentes/Card/index.js
@@ -7,14 +7,25 @@ export default function Card({ item }) {
     return (
         <Link to={item.link} target={item.target}>
             <div className={styles.card}>
+
                 <img
                     className={styles.capa}
                     src={`/assets/${item.pathAssetsImage}/${item.id}/capa.png`}
                     alt='Imagem de Capa do Card'
                 />
+
                 <h2 className={styles.titulo}>{item.titulo}</h2>
-                <p>{item.descricao}</p>
-                <BotaoPrincipal>Ler</BotaoPrincipal>
+
+                {item.descricao ?
+                    <p className={styles.descricao}>
+                        {item.descricao.substring(0, 28)}...
+                    </p>
+                    :
+                    ""}
+
+                {item.botao ?
+                    <BotaoPrincipal>{item.botao}</BotaoPrincipal>
+                    : ""}
             </div>
         </Link>
     )
